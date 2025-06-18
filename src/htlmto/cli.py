72510d2e9049ff2f768da606d9d670d4b7de9c8b
balaranjan <balaranjan@outlook.com>
2025-06-18 16:35:05 -0400

    cohp dev

--- src/htlmto/cli.py
+++ src/htlmto/cli.py
@@ -1,40 +1,18 @@
 from .lmto_core_functions import run_lmto
 from .utilities import extract_data_from_cif
+import multiprocessing as mp
 import argparse
 import os
 
-# if len(sys.argv) != 2:
-#     print(
-#         "Please provide the path to a directory \
-#             containing .cif files as the only argument."
-#     )
 
-
-# # MP
-# def run_lmto_aux(*args):
-#     for arg in args:
-#         run_lmto(**arg)
-
-
-# cif_names = [c for c in os.listdir(sys.argv[1]) if c.endswith(".cif")]
-
-# total = len(cif_names)
-# tasks = []
-
-# for i, cname in enumerate(cif_names, 0):
-#     cif = cname.split(os.sep)[-1]
-
-#     if cif.endswith(".cif"):
-#         cif_data = extract_data_from_cif(f"{sys.argv[1]}{os.sep}{cif}")
-#         cif_data["calc_path"] = sys.argv[1]
-#         cif_data["cif_path"] = f"{sys.argv[1]}{os.sep}{cif}"
-#         tasks.append(cif_data)
-
-# with mp.Pool(processes=2) as p:
-#     p.map(func=run_lmto_aux, iterable=tasks)
-
-# p.close()
-# p.join()
+# MP
+def run_lmto_aux(*args):
+    for arg in args:
+        try:
+            run_lmto(**arg)
+        except Exception as e:
+            print(f"Error while running {arg['name']}")
+            print(e)
 
 
 def main():
@@ -49,15 +27,66 @@ def main():
         description=description, formatter_class=argparse.RawTextHelpFormatter
     )
     parser.add_argument("input_path", help="Path to the input file / folder")
+    parser.add_argument("--force-O2", help="Force origin choice 2")
+    parser.add_argument(
+        "-n",
+        "--num-cores",
+        help="Number of CPU cores to use. \
+                        Default is 2.",
+        type=int,
+        default=2,
+    )
 
     args = parser.parse_args()
-    cif_data = extract_data_from_cif(args.input_path)
 
-    if cif_data:
-        cif_data["calc_path"] = os.getcwd()
-        cif_data["cif_path"] = os.path.join(os.getcwd(), args.input_path)
+    if os.path.isfile(args.input_file):
+        cif_data = extract_data_from_cif(args.input_path)
 
-        run_lmto(**cif_data)
+        if args.force_O2:
+            cif_data["origin"] = 2
+
+        if cif_data:
+            cif_data["calc_path"] = os.getcwd()
+            cif_data["cif_path"] = os.path.join(os.getcwd(), args.input_path)
+
+            run_lmto(**cif_data)
+
+    elif os.path.isdir(args.input_path):
+        if args.force_O2:
+            print(
+                "Origin choice 2 will be used for all files! \
+                  \nRestart the calculation without --force-O2 flag \
+                  if this is not intended."
+            )
+
+        cif_names = [
+            c for c in os.listdir(args.input_path) if c.endswith(".cif")
+        ]
+
+        tasks = []
+        full_path = os.path.join(os.chdir, args.input_path)
+        for i, cname in enumerate(cif_names, 0):
+            cif = cname.split(os.sep)[-1]
+
+            if cif.endswith(".cif"):
+                cif_data = extract_data_from_cif(f"{full_path}{os.sep}{cif}")
+                cif_data["calc_path"] = full_path
+                cif_data["cif_path"] = f"{full_path}{os.sep}{cif}"
+                if args.force_O2:
+                    cif_data["origin"] = 2
+                tasks.append(cif_data)
+
+        with mp.Pool(processes=min(mp.cpu_count(), args.num_cores)) as p:
+            p.map(func=run_lmto_aux, iterable=tasks)
+
+        p.close()
+        p.join()
+
+    else:
+        print(
+            "The input path given is not recognized as a file or directory.\
+               \nPlease check."
+        )
 
 
 if __name__ == "__main__":
